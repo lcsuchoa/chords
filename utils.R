2fa91598bdefe9e4962491d42fc34a0c9bea6bcb
Lucas Uchoa <lucas.uchoa@g.globo>
2023-08-11 11:36:54 -0300

    ajuste na função transpose_chords e avanço no tf-idf

--- utils.R
+++ utils.R
@@ -111,8 +111,7 @@ transpose_capo <- function(data) {
     mutate(key = aux$transposed)
 }
 
-
-
+# função com rowwise
 transpose_chords <- function(data, to_key = "C") {
   
   notes <- c("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
@@ -120,9 +119,10 @@ transpose_chords <- function(data, to_key = "C") {
   major_scale <- c("C", "Dm", "Em", "F", "G", "Am", "Bm")
   
   aux <- data %>%
+    rowwise() %>%
     mutate(
       chord1 = case_when(
-        grepl("/", chord) & !grepl("[0-9]M?-?/[0-9]", chord) ~ strsplit(chord, "/")[[1]][1],
+        grepl("/", chord) & !grepl("[0-9]M?-?/[0-9]", chord) ~ str_split(chord, "/")[[1]][1],
         TRUE ~ chord
       ),
       chord2 = case_when(
@@ -173,6 +173,74 @@ transpose_chords <- function(data, to_key = "C") {
     mutate(
       chordC = aux$transposed,
       keyC = ifelse(grepl("m", key), "Am", "C")
-      # degreeC = 
+    )
+}
+
+
+
+# --------------------------
+# tentativa para tirar rowwise e deixar mais eficiente
+# ainda não está funcionando (erro com acordes tipo G/B)
+
+transpose_chords <- function(data, to_key = "C") {
+  
+  notes <- c("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+  minor <- c("Am", "A#m", "Bm", "Cm", "C#m", "Dm", "D#m", "Em", "Fm", "F#m", "Gm", "G#m")
+  major_scale <- c("C", "Dm", "Em", "F", "G", "Am", "Bm")
+  
+  aux <- data %>%
+    mutate(
+      chord1 = case_when(
+        str_detect(chord, "/") & !str_detect(chord, "[0-9]M?-?/[0-9]") ~ str_split(chord, "/")[[1]][1],
+        TRUE ~ chord
+      ),
+      chord2 = case_when(
+        str_detect(chord, "/") & !str_detect(chord, "[0-9]M?-?/[0-9]") ~ strsplit(chord, "/")[[1]][2],
+        TRUE ~ NA
+      ),
+      main1 = case_when(
+        str_detect(chord1, "#") ~ substr(chord1, 1, 2),
+        !str_detect(chord1, "#") ~ substr(chord1, 1, 1)
+      ),
+      main2 = case_when(
+        is.na(chord2) ~ NA,
+        str_detect(chord2, "#") ~ substr(chord2, 1, 2),
+        !str_detect(chord2, "#") ~ substr(chord2, 1, 1)        
+      ),
+      rest1 = case_when(
+        str_detect(chord1, "#") ~ substr(chord1, 3, nchar(chord1)),
+        !str_detect(chord1, "#") ~ substr(chord1, 2, nchar(chord1))
+      ),
+      rest2 = case_when(
+        is.na(chord2) ~ NA,
+        str_detect(chord2, "#") ~ substr(chord2, 3, nchar(chord2)),
+        !str_detect(chord2, "#") ~ substr(chord2, 2, nchar(chord2))
+      ),
+      from_index = case_when(
+        key %in% notes ~ match(key, notes),
+        key %in% minor ~ match(key, minor)
+      ),
+      to_index = match(to_key, notes),
+      offset = to_index - from_index,
+      chord_index1 = match(main1, notes),
+      chord_index2 = match(main2, notes),
+      transposed_index1 = case_when(
+        chord_index1 + offset > 0 ~ chord_index1 + offset,
+        TRUE ~ chord_index1 + offset + length(notes)
+      ),
+      transposed_index2 = case_when(
+        chord_index2 + offset > 0 ~ chord_index2 + offset,
+        TRUE ~ chord_index2 + offset + length(notes)
+      ),
+      transposed = case_when(
+        is.na(chord2) ~ paste0(notes[transposed_index1], rest1),
+        TRUE ~ paste0(notes[transposed_index1], rest1, "/", notes[transposed_index2], rest2)
+      )
+    )
+  
+  data %>%
+    mutate(
+      chordC = aux$transposed,
+      keyC = ifelse(str_detect(key, "m"), "Am", "C")
     )
 }
